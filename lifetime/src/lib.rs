51cc4b0557a81f66a02256502de1c83b3e792927
Lashi <lashi0812@gmail.com>
2024-11-08 04:52:59 +0000

    handle the tail edge case

--- lifetime/src/lib.rs
+++ lifetime/src/lib.rs
@@ -1,15 +1,14 @@
 #![allow(dead_code)]
 
-
 struct StrSplit<'a> {
-    remainder: &'a str,
+    remainder: Option<&'a str>,
     delimiter: &'a str,
 }
 
 impl<'a> StrSplit<'a> {
     fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
-            remainder: haystack,
+            remainder: Some(haystack),
             delimiter,
         }
     }
@@ -18,18 +17,16 @@ impl<'a> StrSplit<'a> {
 impl<'a> Iterator for StrSplit<'a> {
     type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next_delimiter) = self.remainder.find(self.delimiter) {
-            let until_delimiter = &self.remainder[..next_delimiter];
-            self.remainder = &self.remainder[next_delimiter + self.delimiter.len()..];
-            Some(until_delimiter)
-        } else {
-            if self.remainder.is_empty() {
-                None
+        if let Some(ref mut remainder) = self.remainder {
+            if let Some(next_delimiter) = remainder.find(self.delimiter) {
+                let until_delimiter = &remainder[..next_delimiter];
+                *remainder = &remainder[next_delimiter + self.delimiter.len()..];
+                Some(until_delimiter)
             } else {
-                let rest = self.remainder;
-                self.remainder = "";
-                Some(rest)
+                self.remainder.take()
             }
+        } else {
+            None
         }
     }
 }
@@ -39,4 +36,11 @@ fn it_works() {
     let haystack = "a b c d e";
     let letters = StrSplit::new(haystack, " ");
     assert!(letters.eq(vec!["a", "b", "c", "d", "e"].into_iter()));
+}
+
+#[test]
+fn tail() {
+    let haystack = "a b c d ";
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", ""]);
 }
